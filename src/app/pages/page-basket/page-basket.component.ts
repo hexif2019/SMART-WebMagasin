9ac75c7d11f77369dea4ebdf28a533fc36a0f763
Unknown <hugues.vogel@hotmail.fr>
2018-05-02 07:55:27 +0200

    wip

--- src/app/pages/page-basket/page-basket.component.ts
+++ src/app/pages/page-basket/page-basket.component.ts
@@ -16,6 +16,31 @@ export class PageBasketComponent implements OnInit {
   panier: Commande;
   infoArticles : {article:Article, magasin:Magasin}[];
 
+  paypalCfg = {
+
+    env: 'sandbox', // 'production' or 'sandbox'
+
+    commit: true, // Show a 'Pay Now' button
+
+    payment: function() {
+      return paypal.request.post(CREATE_PAYMENT_URL).then(function(data) {
+        return data.id;
+      });
+    },
+
+    onAuthorize: function(data) {
+      return paypal.request.post(EXECUTE_PAYMENT_URL, {
+        paymentID: data.paymentID,
+        payerID:   data.payerID
+      }).then(function() {
+
+        // The payment is complete!
+        // You can now show a confirmation message to the customer
+      });
+    }
+
+  };
+
   msgError(msg: string){
     console.log(msg);
   }
